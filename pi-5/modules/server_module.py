c1ba6f5457d46f79ffcceb17e360a154da20679e
Serpyne <robbingchew@gmail.com>
2024-06-08 11:06:38 +0100

    pi

--- pi-5/modules/server_module.py
+++ pi-5/modules/server_module.py
@@ -3,18 +3,38 @@ from threading import Thread
 
 class Server:
     def __init__(self, address: str, port: int):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.bind((address, port))
-        self._socket.listen(2)
+        self._address = address
+        self._port = port
+        
+        self.connections = {}
+
+    def on_new_connection(self, connection, address):
+        print(f"Connected with {address}")
+        
+        while True:
+            data = connection.recv(1024)
+            
+            print(address, ">>", data.decode())
+            connection.send(data)
 
     def _run(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.bind((self._address, self._port))
+        self._socket.listen(5)
+        
         while True:
             connection, address = self._socket.accept()
-            buffer = connection.recv(64)
-
-            if len(buffer) > 0:
-                print(buffer.decode())
-
+            
+            thread = Thread(target=self.on_new_connection, args=(connection, address,))
+            thread.start()
+            self.connections[address] = thread
+        
+        
     def start(self):
         self._thread = Thread(target=self._run)
         self._thread.start()
+        
+if __name__ == "__main__":
+    
+    server = Server("192.168.1.34", 8089)
+    server.start()
