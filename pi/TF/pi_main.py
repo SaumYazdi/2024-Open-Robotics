263397303164888ab1c5164a72d0d6f338ba3927
Serpyne <robbingchew@gmail.com>
2024-07-19 17:55:27 +0100

    I HATE PI5

--- pi/TF/pi_main.py
+++ pi/TF/pi_main.py
@@ -3,15 +3,22 @@
 from ball import BallDetector
 from camera import PCCamera
 import cv2 as cv
+from os.path import join, dirname
 
-from modules.fan_module import Fan
-from modules.camera_module import Camera
+def pathname(filename):
+    return join(dirname(__file__), filename)
+    
+import sys
+sys.path.insert(1, "/home/che0307/Documents/2024-Open-Robotics/pi/modules")
+
+from fan_module import Fan
+from camera_module import Camera
 
 if __name__ == "__main__":
     fan = Fan()
     fan.on()
 
-    camera_processing = PCCamera(active=False)
+    camera_processing = PCCamera()
 
     camera = Camera()
     detector = BallDetector('transformed_frozen_inference_graph.pb', 'ssd_mobilenet_v1_balls.pbtxt')
