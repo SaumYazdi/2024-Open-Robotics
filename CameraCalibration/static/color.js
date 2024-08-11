cb2fdb9d21b274c19315cd38b0bb6b928cbe77b6
Serpyne <robbingchew@gmail.com>
2024-08-11 19:56:19 +1000

    PC

---
+++ CameraCalibration/static/color.js
@@ -0,0 +1,83 @@
+
+let colorsLabel = getE("colors");
+let colorSwitch = getE("color-select");
+
+function extractPixelColor(cols, x, y) {
+    let pixel = cols * parseInt(x) + parseInt(y);
+    let position = pixel * 4;
+    return {
+        red: imageData[position],
+        green: imageData[position + 1],
+        blue: imageData[position + 2],
+        alpha: imageData[position + 3],
+    };
+};
+
+function colorSelect() {
+    selectedColors = [];
+    colorsLabel.innerHTML = `Selecting Colours: ${colorSwitch.checked}`;
+}
+
+function calibrateColors() {
+    if (selectedColors.length == 0)
+        return;
+       
+    fetch("/calibrateColors", {
+        method: "POST",
+        body: JSON.stringify(selectedColors),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .catch(error => {return 0;});
+}
+
+var selectedColors = [];
+var pixelColor;
+var imageData;
+function setPreview(src) {
+    if (visible == false)
+        return null;
+        
+    let img = new window.Image();
+    img.crossOrigin = `Anonymous`;
+    
+    img.src = src;
+    img.onload = function() {
+    
+        preview.width = img.width;
+        preview.height = img.height;
+        
+        previewContext = preview.getContext("2d", willReadFrequently = true);
+        previewContext.drawImage(img, 0, 0);
+        
+        imageData = previewContext.getImageData(0, 0, preview.width, preview.height).data;
+        
+        preview.onmousemove = (event) => {
+            if (!colorSwitch.checked)
+                return;
+                
+            let cols = preview.width;
+            let rect = preview.getBoundingClientRect();
+            let scaleX = preview.width / rect.width;
+            let scaleY = preview.height / rect.height;
+            let x = (event.clientX - rect.left) * scaleX;
+            let y = (event.clientY - rect.top) * scaleY;
+            
+            let c = extractPixelColor(cols, y, x);
+            pixelColor = `(${c.red}, ${c.green}, ${c.blue})`;
+            previewContext.fillStyle = "white";
+            previewContext.fillRect(0, preview.height - 50, 50, 50);
+            previewContext.fillStyle = "rgb" + pixelColor;
+            previewContext.fillRect(5, preview.height - 45, 40, 40);
+        };
+    };
+}
+
+preview.addEventListener("click", () => {
+    if (!colorSwitch.checked)
+        return;
+    selectedColors.push(pixelColor);
+    colorsLabel.innerHTML = `Selecting Colours: true, Colors: ${selectedColors.join(', ')}`;   
+});
+      
--- CameraCalibration/static/index.js
+++ CameraCalibration/static/index.js
@@ -17,8 +17,6 @@ let updateSlider = getE("update-slider");
 let previewFrame = getE("preview-frame");
 let preview = getE("preview");
 let previewButton = getE("preview-button");
-let colorsLabel = getE("colors");
-let colorSwitch = getE("color-select");
 
 let detectedLabel = getE("detected-label");
 let radiusLabel = getE("radius-label");
@@ -53,8 +51,8 @@ var angle = 0;
 var k, a;
 var m, c;
 
-var updating = true;
-var visible = false;
+var updating = true; // Preview updates
+var visible = false; // Preview visibility
 
 function showPreview() {
     let text = previewButton.innerHTML;
@@ -75,86 +73,7 @@ function showPreview() {
             .catch(error => {return 0;});
     }
 }
-
-function extractPixelColor(cols, x, y) {
-    let pixel = cols * parseInt(x) + parseInt(y);
-    let position = pixel * 4;
-    return {
-        red: imageData[position],
-        green: imageData[position + 1],
-        blue: imageData[position + 2],
-        alpha: imageData[position + 3],
-    };
-};
-
-function colorSelect() {
-    selectedColors = [];
-    colorsLabel.innerHTML = `Selecting Colours: ${colorSwitch.checked}`;
-}
-
-function calibrateColors() {
-    if (selectedColors.length == 0)
-        return;
-       
-    fetch("/calibrateColors", {
-        method: "POST",
-        body: JSON.stringify(selectedColors),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    .catch(error => {return 0;});
-}
-
-var selectedColors = [];
-var pixelColor;
-var imageData;
-function setPreview(src) {
-    if (visible == false)
-        return null;
-        
-    let img = new window.Image();
-    img.crossOrigin = `Anonymous`;
-    
-    img.src = src;
-    img.onload = function() {
-    
-        preview.width = img.width;
-        preview.height = img.height;
-        
-        previewContext = preview.getContext("2d", willReadFrequently = true);
-        previewContext.drawImage(img, 0, 0);
-        
-        imageData = previewContext.getImageData(0, 0, preview.width, preview.height).data;
-        
-        preview.onmousemove = (event) => {
-            if (!colorSwitch.checked)
-                return;
-                
-            let cols = preview.width;
-            let rect = preview.getBoundingClientRect();
-            let scaleX = preview.width / rect.width;
-            let scaleY = preview.height / rect.height;
-            let x = (event.clientX - rect.left) * scaleX;
-            let y = (event.clientY - rect.top) * scaleY;
-            
-            let c = extractPixelColor(cols, y, x);
-            pixelColor = `(${c.red}, ${c.green}, ${c.blue})`;
-            previewContext.fillStyle = "white";
-            previewContext.fillRect(0, preview.height - 50, 50, 50);
-            previewContext.fillStyle = "rgb" + pixelColor;
-            previewContext.fillRect(5, preview.height - 45, 40, 40);
-        };
-    };
-}
-
-preview.addEventListener("click", () => {
-    if (!colorSwitch.checked)
-        return;
-    selectedColors.push(pixelColor);
-    colorsLabel.innerHTML = `Selecting Colours: true, Colors: ${selectedColors.join(', ')}`;   
-});
-        
+  
 ticks = 0;
 function update() {
     if (updating && (ticks % Math.round(MEASURE_INTERVAL / 100) == 0)) {
